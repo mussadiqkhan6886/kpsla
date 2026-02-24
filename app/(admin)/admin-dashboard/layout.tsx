7516075dd699b53ad4eb69f7be64e7512e7fbb2e
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-02-24 21:06:28 +0500

    website structure

--- app/(admin)/admin-dashboard/layout.tsx
+++ app/(admin)/admin-dashboard/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,12 +11,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    {children}
   );
 }
---
+++ app/(admin)/admin-dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminHome = () => {
+  return (
+    <main>
+      
+    </main>
+  )
+}
+
+export default AdminHome
